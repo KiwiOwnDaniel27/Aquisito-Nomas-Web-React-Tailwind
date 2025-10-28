fc95627152c0e104c2bb76abf74d19649617573a
Daniel Quiguango <fiel2589@hotmail.com>
2025-10-27 21:30:04 -0500

    cambio final links

--- src/data.js
+++ src/data.js
@@ -71,7 +71,7 @@ export const listProyek = [
     desk: "Puedes realiazar pagos con cualquier cuenta bancaria nacional o internacional",
     tools: ["Débito", "Transferencia"],
     dad: "200",
-    link: "https://drive.google.com/file/d/1Qx7zeJvboRrgdWFOI-Ac1mM1JC2dvIYh/view?usp=sharing" ,
+    link: "https://drive.google.com/file/d/1BM99TQvZkk5GNcaij4Y2t6QVFg1qF9Mc/view?usp=sharing" ,
   },
   {
     id: 2,
@@ -80,6 +80,6 @@ export const listProyek = [
     desk: "Reserva y puedes pagar en efecivo cuando lo retires",
     tools: ["Dolares"],
     dad: "300",
-    link: "https://drive.google.com/file/d/1Qx7zeJvboRrgdWFOI-Ac1mM1JC2dvIYh/view?usp=sharing" ,
+    link: "https://drive.google.com/file/d/1BM99TQvZkk5GNcaij4Y2t6QVFg1qF9Mc/view?usp=sharing" ,
   },
 ];
